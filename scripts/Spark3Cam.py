1d0f123fda340e18bb615e8d3c82d38aa432bc80
batuogkoc <bgunduz21@ku.edu.tr>
2022-04-14 22:25:20 +0300

    masked out hood of car and removed corner detection code

--- scripts/Spark3Cam.py
+++ scripts/Spark3Cam.py
@@ -6,6 +6,7 @@ import Panorama
 from sensor_msgs.msg import CompressedImage
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped
+import std_msgs.msg
 import geometry_msgs
 import tf2_ros
 import tf.transformations as ts
@@ -17,6 +18,7 @@ import time
 import math as m
 from copy import deepcopy
 from utils import *
+from matplotlib import pyplot as plt
 
 tf_buffer = tf2_ros.Buffer()
 tf_listener = None
@@ -27,12 +29,14 @@ main_camera_transform = None
 left_camera_transform = None
 right_camera_transform = None
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-# out = cv2.VideoWriter("output.avi", fourcc, 15, (1920, 1080))
+out = cv2.VideoWriter("output.avi", fourcc, 15, (1920, 1080))
 first_time = True
 
 main_time = 0
 right_time = 0
 left_time = 0
+
+rate_control_publisher = None
 
 def left_camera_callback(data):
     frame_id = "left_camera"
@@ -68,23 +72,47 @@ def right_camera_callback(data):
 def main_camera_callback(data):
     frame_id = "main_camera"
     t = rospy.Time.now()
-    times = Times()
     global tf_buffer
     global panorama
     global bridge
     global source
     global main_camera_transform
-    global left_camera_transform
-    global right_camera_transform
 
     try:
         main_camera_transform = tf_buffer.lookup_transform(source, frame_id, rospy.Time(0), rospy.Duration(1.0))
-        img = bridge.compressed_imgmsg_to_cv2(data)
-        panorama.cameras[frame_id].add_image_stamped(t, img)
+        frame = bridge.compressed_imgmsg_to_cv2(data)
+        h, w, d = np.shape(frame)
+        frame_masked = deepcopy(frame)
+        blank = np.zeros((h,w), np.uint8)
+        x_a = 70
+        x_b = 180
+        y_b = 960
+        x_c = 500
+        y_c = 890
+        trapezoid = np.array([[x_a, 1080],
+                              [1250-x_a, 1080],
+                              [1250-x_b, y_b],
+                              [1250-x_c, y_c],
+                              [x_c, y_c],
+                              [x_b, y_b]], np.int32)
+        trapezoid = trapezoid.reshape((-1, 1, 2))
+        cv2.fillPoly(blank, [trapezoid], (255, 255, 255))
+        frame_masked = cv2.bitwise_and(frame_masked, frame_masked, mask=cv2.bitwise_not(blank))
+        panorama.cameras[frame_id].add_image_stamped(t, frame_masked)
     except Exception as e:
         rospy.logerr("couldn't get "+frame_id+" tf: "+ str(e))
 
 
+
+    main()
+
+    
+
+def main():
+    times = Times()
+    global main_camera_transform
+    global left_camera_transform
+    global right_camera_transform
     left_camera_htm = geometry_msgs_TransformStamped_to_htm(left_camera_transform)
     right_camera_htm = geometry_msgs_TransformStamped_to_htm(right_camera_transform)
     main_camera_htm = geometry_msgs_TransformStamped_to_htm(main_camera_transform)
@@ -110,42 +138,32 @@ def main_camera_callback(data):
     right_local_htm = np.matmul(map_tranform_htm_only_z_rot_inv, right_camera_htm)
     left_local_htm = np.matmul(map_tranform_htm_only_z_rot_inv, left_camera_htm)
 
+    # main_local_htm = main_camera_htm
+    # left_local_htm = left_camera_htm
+    # right_local_htm = right_camera_htm
+
     panorama.cameras["left_camera"].set_htm(left_local_htm, left_camera_transform.header.stamp)
     panorama.cameras["right_camera"].set_htm(right_local_htm, right_camera_transform.header.stamp)
     panorama.cameras["main_camera"].set_htm(main_local_htm, main_camera_transform.header.stamp)
     times.add("htm")
     panorama.clear_img()
     panorama.project_all_cameras()
+    # panorama.project_camera("main_camera")
     frame = deepcopy(panorama.get_output_img())
     times.add("project")
-    # h, w, d = np.shape(frame)
-    # frame_masked = deepcopy(frame)
-    # blank = np.zeros((h,w), np.uint8)
-    # trapezoid = np.array([[400, 570],
-    #                     [730, 570],
-    #                     [730, 500],
-    #                     [600, 410],
-    #                     [530, 410],
-    #                     [400, 500]], np.int32)
-    # trapezoid = trapezoid.reshape((-1, 1, 2))
-    # cv2.fillPoly(blank, [trapezoid], (255, 255, 255))
-    # frame_masked = cv2.bitwise_and(frame_masked, frame_masked, mask=cv2.bitwise_not(blank))
-    # right_handed_corners, left_handed_corners, other_corners = find_corners(frame_masked.astype(np.uint8))
 
-    # for corner in right_handed_corners:
-    #     cv2.circle(frame, tuple(corner), 5, (0,0,255),thickness = 3) 
-    # for corner in left_handed_corners:
-    #     cv2.circle(frame, tuple(corner), 5, (0,255,0),thickness = 3) 
-    # for corner in other_corners:
-    #     cv2.circle(frame, tuple(corner), 5, (255,0,0),thickness = 3) 
-    # cv2.imwrite("img.jpg", frame.astype(np.float32))
-    resized_img = imshow_r("a", frame, (1280, 720))
+    # resized_img = imshow_r("a", frame, (1920, 1080))
+    cv2.imshow("frame", panorama.cameras["main_camera"].get_image_with_closest_stamp_to_htm())
     cv2.waitKey(1)
     global first_time
 
     # out.write(resized_img)
     times.add("imshow")
     print(times)
+    rate_control_string = std_msgs.msg.String()
+    rate_control_string.data = "Done"
+    global rate_control_publisher
+    rate_control_publisher.publish(rate_control_string)
     # print(1/duration_to_sec(rospy.Time.now()-t))
 
 def node():
@@ -159,6 +177,11 @@ def node():
     rospy.Subscriber("left_camera/image/compressed", CompressedImage, left_camera_callback)
     rospy.Subscriber("right_camera/image/compressed", CompressedImage, right_camera_callback)
 
+    global rate_control_publisher
+    rate_control_publisher = rospy.Publisher("/rate_control", std_msgs.msg.String, queue_size=10)
+    rate_control_string = std_msgs.msg.String()
+    rate_control_string.data = "Done"
+    rate_control_publisher.publish(rate_control_string)
     panorama = Panorama.Panorama(0.03)
 
     get_tf = True
@@ -168,19 +191,7 @@ def node():
             left_camera_transform = tf_buffer.lookup_transform("map", "left_camera", rospy.Time(0), rospy.Duration(1.0))
             right_camera_transform = tf_buffer.lookup_transform("map", "right_camera", rospy.Time(0), rospy.Duration(1.0))
             
-            # f = (1080/2)*m.tan(m.pi*60/180)
-            # l_r_fov = 2*m.atan(1920/2/f)
-            # main_fov = 2*m.atan(1250/2/f)
-
-            # pos,rot = geometry_msgs_TransformStamped_to_pos_rot(main_camera_transform)
-            # panorama.add_camera("main_camera", pos, rot, 1250, 1080, main_fov)
-
-            # pos,rot = geometry_msgs_TransformStamped_to_pos_rot(left_camera_transform)
-            # panorama.add_camera("left_camera", pos, rot, 1920, 1080, l_r_fov)
-
-            # pos,rot = geometry_msgs_TransformStamped_to_pos_rot(right_camera_transform)
-            # panorama.add_camera("right_camera", pos, rot, 1920, 1080, l_r_fov)
-            a = 0.66
+            a = 0.6
             left_camera = Panorama.Camera(1920, 1080, m.pi*60/180, 30, "left_camera")
             left_camera.set_orthogonal_roi(0, left_camera.width, int(left_camera.height*(a)), left_camera.height)
             right_camera = Panorama.Camera(1920, 1080, m.pi*60/180, 30, "right_camera")
